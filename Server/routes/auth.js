13b78590056125e62f0e58069bbba1ffbd428b82
Abdullah Tanveer <abdullahtanveer56@outlook.com>
2021-12-27 02:19:56 +0500

    added new routes
    added routes for expense, category and wallet

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { register, login, forgetPassword } = require("../controllers/auth");
+const {
+  register,
+  login,
+  forgetPassword,
+  resetPassword,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgetpassword", forgetPassword);
+router.post("/resetpassword", resetPassword);
 
 module.exports = router;
--- Server/routes/private.js
+++ Server/routes/private.js
@@ -2,7 +2,29 @@ const express = require("express");
 const router = express.Router();
 const { privateRoute } = require("../controllers/private");
 const { protect } = require("../middleware/auth");
+const {
+  addExpense,
+  deleteExpense,
+  updateExpense,
+} = require("../controllers/expense");
+const {
+  addCategory,
+  deleteCategory,
+  updateCategory,
+} = require("../controllers/category");
+const { cashDeposit } = require("../controllers/wallet");
 
-router.get("/private",protect, privateRoute);
+// protected routes
+router.get("/private", protect, privateRoute);
+// manage wallet
+router.post("/wallet/deposit", protect, cashDeposit);
+// manage expenses
+router.post("/wallet/expense", protect, addExpense);
+router.delete("/wallet/expense?id", protect, deleteExpense);
+router.put("/wallet/expense", protect, updateExpense);
+// manage user categories
+router.post("/wallet/category", protect, addCategory);
+router.delete("/wallet/category?id", protect, deleteCategory);
+router.put("/wallet/category", protect, updateCategory);
 
 module.exports = router;
